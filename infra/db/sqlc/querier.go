d2ffaaadce4f4c7cfe0ce8ed10d1f1ab344c8680
Pedro Gomes <pcgbernardes@gmail.com>
2024-01-27 16:00:42 -0400

    feat: add entry queries and repositories

--- infra/db/sqlc/querier.go
+++ infra/db/sqlc/querier.go
@@ -10,9 +10,15 @@ import (
 
 type Querier interface {
 	CreateAccount(ctx context.Context, arg CreateAccountParams) (Account, error)
+	CreateEntry(ctx context.Context, arg CreateEntryParams) (Entry, error)
+	CreateTransaction(ctx context.Context, arg CreateTransactionParams) (Transaction, error)
 	DeleteAccount(ctx context.Context, id int64) error
 	GetAccount(ctx context.Context, id int64) (Account, error)
-	ListAccounts(ctx context.Context, arg ListAccountsParams) (Account, error)
+	GetEntry(ctx context.Context, id int64) (Entry, error)
+	GetTransaction(ctx context.Context, id int64) (Transaction, error)
+	ListAccounts(ctx context.Context, arg ListAccountsParams) ([]Account, error)
+	ListEntries(ctx context.Context, arg ListEntriesParams) ([]Entry, error)
+	ListTransactions(ctx context.Context, arg ListTransactionsParams) ([]Transaction, error)
 	UpdateAccount(ctx context.Context, arg UpdateAccountParams) (Account, error)
 }
 
