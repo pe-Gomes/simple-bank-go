83062344c93c07ddfca99cb2ec6238e760526b93
Pedro Gomes <pcgbernardes@gmail.com>
2024-02-14 11:15:42 -0400

    feat: update user test to use hashed password

--- infra/db/repository/user_test.go
+++ infra/db/repository/user_test.go
@@ -10,9 +10,12 @@ import (
 )
 
 func createRandomUser(t *testing.T) User {
+	hashedPass, err := util.HashPassword(util.RandomString(6))
+	require.NoError(t, err)
+
 	arg := CreateUserParams{
 		Username:       util.RandomOwner(),
-		HashedPassword: "secret",
+		HashedPassword: hashedPass,
 		FullName:       util.RandomOwner(),
 		Email:          util.RandomEmail(),
 	}
