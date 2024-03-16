ee581a1e7c54a4ce2ba30bb6a593d621497f5029
Pedro Gomes <pcgbernardes@gmail.com>
2024-03-16 16:08:50 -0400

    feat: update main and env to new store requirements

--- main.go
+++ main.go
@@ -24,7 +24,11 @@ func main() {
 	}
 
 	store := db.NewStore(connPool)
-	server := api.NewServer(store)
+	server, err := api.NewServer(config, store)
+	
+	if err != nil {
+		log.Fatal("Could not create server: ", err)
+	}
 
 	err = server.Start(config.ServerAddress)
 
