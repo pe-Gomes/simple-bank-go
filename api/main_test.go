d3073aca999d9e6c440954bca6b2bf709612b2f1
Pedro Gomes <pcgbernardes@gmail.com>
2024-03-16 16:09:10 -0400

    feat: add main test

---
+++ api/main_test.go
@@ -0,0 +1,30 @@
+package api
+
+import (
+	"os"
+	"testing"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	db "github.com/pe-Gomes/simple-bank-go/infra/db/repository"
+	"github.com/pe-Gomes/simple-bank-go/util"
+	"github.com/stretchr/testify/require"
+)
+
+func NewTestServer(t *testing.T, store db.Store) *Server {
+	config := util.Config{
+		TokenSymmetricKey:   util.RandomString(32),
+		AccessTokenDuration: time.Minute,
+	}
+
+	server, err := NewServer(config, store)
+	require.NoError(t, err)
+
+	return server
+}
+
+func TestMain(m *testing.M) {
+	gin.SetMode(gin.TestMode)
+
+	os.Exit(m.Run())
+}
