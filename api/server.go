58fe9f3f487652e9b225c4a7da7848722601f891
Pedro Gomes <pcgbernardes@gmail.com>
2024-03-17 16:56:28 -0400

    feat: add middleware to desired routes

--- api/server.go
+++ api/server.go
@@ -42,14 +42,16 @@ func NewServer(config util.Config, store db.Store) (*Server, error) {
 func (server *Server) setupRouter() {
 	router := gin.Default()
 
-	router.POST("/accounts", server.createAccount)
-	router.GET("/accounts/:id", server.getAccount)
-	router.GET("/accounts", server.listAccounts)
-
-	router.POST("/transactions", server.createTransaction)
-
-	router.POST("/users", server.createUser)
 	router.POST("/users/login", server.loginUser)
+	router.POST("/users", server.createUser)
+
+	authRoutes := router.Group("/").Use(authMiddleware(server.tokenMaker))
+
+	authRoutes.POST("/accounts", server.createAccount)
+	authRoutes.GET("/accounts/:id", server.getAccount)
+	authRoutes.GET("/accounts", server.listAccounts)
+
+	authRoutes.POST("/transactions", server.createTransaction)
 
 	server.router = router
 }
